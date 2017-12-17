96f8ad4d8dacd989fd9b39b310f0e77c1e150db7
haran <haran@ieee.org>
2017-12-16 22:50:36 -0800

    first run wo twiddle

--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,25 @@ std::string hasData(std::string s) {
   return "";
 }
 
-int main()
+//int main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
   uWS::Hub h;
 
   PID pid;
   // TODO: Initialize the pid variable.
+    
+    double Kp,Ki,Kd;
+    
+//    Kp = atof(argv[0]);
+//    Ki = atof(argv[1]);
+//    Kd = atof(argv[2]);
+    
+    Kp = -0.75;
+    Ki = -0.004;
+    Kd = -1;
+    
+    pid.Init(Kp, Ki, Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,6 +70,10 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
+            
+            pid.UpdateError(cte);
+            
+            steer_value = pid.TotalError();
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
