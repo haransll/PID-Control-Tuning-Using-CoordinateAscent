f82b6f9fbcffa70ce452b35b901c7a7ab816ed3b
haran <haran@ieee.org>
2018-02-14 22:12:25 -0800

    second commit

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,37 +1,33 @@
 #include "PID.h"
+#include <math.h>
 
 using namespace std;
 
 /*
-* TODO: Complete the PID class.
-*/
+ * TODO: Complete the PID class.
+ */
 
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
-    
-    this->Kp = Kp;
-    this->Kd = Kd;
-    this->Ki = Ki;
-    
-    
-    this->p_error = 0;
-    this->i_error = 0;
-    this->d_error = 0;
+    this->Kp=Kp;
+    this->Ki=Ki;
+    this->Kd=Kd;
+    p_error=0.0;
+    i_error=0.0;
+    d_error=0.0;
+    total_err=0.0;
 }
 
 void PID::UpdateError(double cte) {
-    
-    d_error = cte - p_error;
-    p_error = cte;
+    d_error=cte-p_error;
+    p_error=cte;
     i_error+=cte;
-    
+    total_err+=fabs(cte);
 }
 
 double PID::TotalError() {
-    
-    return Kp*p_error+Ki*i_error+Kd*d_error;
+    return total_err;
 }
-
